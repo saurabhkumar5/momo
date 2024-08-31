22517c4671c0f985c4c10b826c13ebf7bea4e9e3
saurabh kumar <saurabhkumar9493@gmail.com>
2024-08-31 19:46:12 +0530

    momo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+
+import photo from './saurabh.png'
+import video from './car.mp4'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+function App(){
+
+    
+  return(
+    <div>
+      <h1 style={{backgroundColor:"red",color:"yellow"}}>hello from react</h1>
+      <h2>kullu</h2>
+      <a href="https://github.com/saurabhkumar5/batch7pm_reactjs">click here</a>
+      <img src="https://img1.wsimg.com/isteam/ip/1172a89b-094a-415b-99fe-cc1e7d3cd9b0/blob.png/:/cr=t:0%25,l:0%25,w:100%25,h:100%25/rs=w:1500,h:1000,cg:true"/>
+      <img src={photo}/>
+      <video src={video} controls></video>
     </div>
-  );
+   
+  )
+
 }
 
 export default App;
